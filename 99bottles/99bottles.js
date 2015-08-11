ff24a86787a83baf715d06b9e5d9e7370be48301
Kyle Bennett <kyle.j.bennett5@gmail.com>
2015-08-10 21:29:20 -0400

    Initial Commit

---
+++ 99bottles/99bottles.js
@@ -0,0 +1,6 @@
+
+for(var i = 99; i > 0; i--) {
+	console.log((i) + " bottles of root beer on the wall, " + (i) + " bottles of root beer...take one down, pass it around " + (i-1) +" bottles of root beer on the wall,");
+}
+
+
